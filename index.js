27e56f7700f0673c6f096d789466672b27023306
Tim M <tim917@gmail.com>
2019-06-18 09:35:06 +0300

    Ping service

--- index.js
+++ index.js
@@ -99,11 +99,9 @@ function unifiedServer(req, res) {
 // Define the handlers
 var handlers = {};
 
-// Sample handler
-handlers.sample = function(data, callback) {
-  // Callback a http status code, and a payload object
-
-  callback(406, { 'name': 'sample handler' });
+// Ping handler
+handlers.ping = function(data, callback) {
+  callback(200);
 }
 
 // Not found handler
@@ -113,5 +111,5 @@ handlers.notFound = function(data, callback) {
 
 // Define a request router
 var router = {
-  'sample': handlers.sample,
+  'ping': handlers.ping,
 };
