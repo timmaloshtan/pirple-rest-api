518d46fc485873b6313e533ab38bc491a0e07594
Tim M <tim917@gmail.com>
2019-06-18 08:42:28 +0300

    Configuration descerned from NODE_ENV variable

---
+++ config.js
@@ -0,0 +1,32 @@
+/*
+ * Create and export configuration variables
+ * 
+ */
+
+// Container for all the environments
+var environments = {};
+
+// Staging (default) environment
+environments.staging = {
+  port: 3000,
+  envName: 'staging',
+};
+
+// Production environment
+environments.production = {
+  port: 5000,
+  envName: 'production',
+};
+
+// Determine which env was passed as a command-line argument
+var currentEnvironment = typeof process.env.NODE_ENV == 'string'
+  ? process.env.NODE_ENV.toLowerCase()
+  : '';
+
+// Check that current env exists in environments container or default to staging
+var environmentToExport = typeof environments[currentEnvironment] == 'object'
+  ? environments[currentEnvironment]
+  : environments.staging;
+
+// Export the module
+module.exports = environmentToExport;
